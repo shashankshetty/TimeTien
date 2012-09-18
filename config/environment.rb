9904afb112d22245052ad4f10edc766f150fcbb0
shashank <shashank_shetty@yahoo.com>
2012-09-18 00:35:52 -0500

    move gmail smtp settings to development and production environment files

--- config/environment.rb
+++ config/environment.rb
@@ -5,16 +5,3 @@ require File.expand_path('../application', __FILE__)
 Timetien::Application.initialize!
 
 config_time_zone = "Central Time (US & Canada)"
-
-Timetien::Application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-end
-
-ActionMailer::Base.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => "587",
-    :user_name => "timetien@gmail.com",
-    :password => "TimeTien@123",
-    :authentication => :plain
-}
