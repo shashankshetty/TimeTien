396d11e39168ad7c0fc9689a7364d8a3d610a585
shashank <shashank_shetty@yahoo.com>
2012-09-18 00:43:39 -0500

    added host parameter to production environment file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,8 @@ Timetien::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { :host => 'timetien.herokuapp.com' }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
